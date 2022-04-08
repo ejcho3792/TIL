e0718921ca1925cbf66b4b80d2563505f34fe5ce
ejcho3792 <ejcho3792@gmail.com>
2022-04-08 11:39:48 +0900

    add commit

---
+++ react_app/README.md
@@ -0,0 +1,6 @@
+# extention prettier
+
+# npm start
+
+
+
---
+++ react_app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Fragment } from 'react';
+
+function App() {
+  return (
+    <Fragment>
+        <div>함수형</div>
+      <div>,,,,,,,,,,,,?</div>
+    </Fragment>
+  );
+}
+
+export default App;
